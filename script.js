03676310535fdc4a17c40f9260ec54612f12f703
muhammedsubhan <muhammedsubhan692@gmail.com>
2022-11-23 06:34:25 +0500

    Loan Button competed

--- script.js
+++ script.js
@@ -192,3 +192,17 @@ btnClose.addEventListener('click', function (e) {
   }
   inputCloseUsername.value = inputClosePin.value = '';
 });
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+
+    // Update the UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
